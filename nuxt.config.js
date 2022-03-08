a518fa82352f8d67d41ebc250ce51d7e93c5e0cc
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-08 17:06:57 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,5 +88,9 @@ export default {
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
     build: {
-    }
+    },
+    generate: {
+        // dir: '../../../enman.magicgifted.com'
+        dir: '../traveler.magicgifted.com',
+    },
 }
