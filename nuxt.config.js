ffb8c728e84c7e5b9ffc22de4f7d5711946852db
今野龍之介 <hilcrhymer78787@gmail.com>
2022-03-10 15:48:38 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,9 @@ export default {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ],
+        script: [
+            { src: 'https://unpkg.com/pwacompat' }
         ]
     },
 
